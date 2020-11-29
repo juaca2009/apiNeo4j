1fa331a4d4b9ded2a57c3adc7d9836e5548bf45d
camilo hernandez <juaca2009@gmail.com>
2020-11-28 22:57:19 -0500

    dtos corregidos

--- api/dto/createConceptoDto.py
+++ api/dto/createConceptoDto.py
@@ -1,23 +1,17 @@
 class createConceptoDto():
-    def __init__(self, _idConcept, _coddeType, _desc) :
-        self.__idConcept = _idConcept
-        self.__codeType = _coddeType
+    def __init__(self, _nombre, _desc) :
+        self.__nombre = _nombre
         self.__desc = _desc
 
-    def get_idConcept(self):
-        return self.__idConcept
 
-    def get_codeType(self):
-        return self.__codeType
+    def get_nombre(self):
+        return self.__nombre
 
     def get_desc(self):
         return self.__desc
 
-    def set_idConcept(self, _idConcept):
-        self.__idConcept = _idConcept
-
-    def set_codeType(self, _coddeType):
-        self.__codeType = _coddeType
+    def set_nombre(self, _nombre):
+        self.__codeType = _nombre
     
     def set_desc(self, _desc):
         self.__desc = _desc
--- api/dto/createGrupoDto.py
+++ api/dto/createGrupoDto.py
@@ -1,20 +1,13 @@
 class createGrupoDto():
-    def __init__(self, _nombre, _desc, _id):
-        self.__id = _id
+    def __init__(self, _nombre, _desc):
         self.__nombre = _nombre
         self.__desc = _desc
-
-    def get_id(self):
-        return self.__id
 
     def get_nombre(self):
         return self.__nombre
 
     def get_desc(self):
         return self.__desc 
-
-    def set_id(self, _id):
-        self.__id = _id
 
     def set_nombre(self, _nombre):
         self.__nombre = _nombre
