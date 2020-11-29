bf3768f5fb99c75c42f3a14aed45bb75374c457a
camilo hernandez <juaca2009@gmail.com>
2020-11-28 22:51:18 -0500

    eliminado repo conceptos

--- api/repository/CGroupRepository.py
+++ api/repository/CGroupRepository.py
@@ -12,48 +12,58 @@ class CGroupRepository():
         self.__driver = GraphDatabase.driver(self.__host, auth=(self.__usuario, self.__password))
         self.__sesion = self.__driver.session()
 
-    def crear_grupo(self, _nombre, _desc, _id):
+    def crear_grupo(self, _nombre, _desc):
         query  = "CREATE (p:Grupo"
-        query = query + " {id:$id, nombre:$nom, descripcion:$des})"
-        self.__sesion.run(query, parameters = {'id':_id, 'nom':_nombre, 'des':_desc})
+        query = query + " {nombre:$nom, descripcion:$des})"
+        self.__sesion.run(query, parameters = {'nom':_nombre, 'des':_desc})
 
-    def eliminar_grupo(self, _id):
-        query = "MATCH(n:Grupo {id:$id}) DETACH DELETE n"
-        self.__sesion.run(query, parameters = {'id':_id})
+    def eliminar_grupo(self, _nombre):
+        query = "MATCH(n:Grupo {nombre:$nom}) DETACH DELETE n"
+        self.__sesion.run(query, parameters = {'nom':_nombre})
         query = "MATCH (n) WHERE NOT (n)--() DELETE n"
         self.__sesion.run(query)
 
-    def crear_concepto(self,  _codeType, _idConcept, _desc):
+    def crear_concepto(self,  _nombre, _desc):
         query = "CREATE (p:Concepto"
-        query = query + " {id_concepto:$id, id_type:$conid, descripcion:$des})"
-        self.__sesion.run(query, parameters = {'id':_idConcept, 'conid':_idConcept, 'des':_desc})
+        query = query + " {nombre:$nom, descripcion:$des})"
+        self.__sesion.run(query, parameters = {'nom':_nombre, 'des':_desc})
 
-    def agregar_concepto(self, _id, _codeType, _idConcept, _desc):
-        self.crear_concepto(_codeType, _idConcept, _desc)
-        query = "MATCH (g:Grupo {id:$id}), (c:Concepto {id_concepto:$idc}) CREATE (g)-[:contiene_a]->(c)"
-        self.__sesion.run(query, parameters = {'id':_id, 'idc':_idConcept})
+    def agregar_concepto(self, _nombreg, _nombrec, _desc):
+        self.crear_concepto(_nombrec, _desc)
+        query = "MATCH (g:Grupo {nombre:$nomg}), (c:Concepto {nombre:$nomc}) CREATE (g)-[:contiene_a]->(c)"
+        self.__sesion.run(query, parameters = {'nomg':_nombreg, 'nomc':_nombrec})
 
-    def eliminar_concepto(self, _idConcept):
-        query = "MATCH(n:Concepto {id_concepto:$idc}) DETACH DELETE n"
-        self.__sesion.run(query, parameters = {'idc': _idConcept})
+    def eliminar_concepto(self, _nombre):
+        query = "MATCH(n:Concepto {nombre:$nom}) DETACH DELETE n"
+        self.__sesion.run(query, parameters = {'nom': _nombre})
 
-    def obtener_grupo(self, _id):
-        query = "MATCH(n:Grupo) WHERE n.id = $id RETURN n.nombre"
-        a = self.__sesion.run(query, parameters = {'id':_id})
-        return a.single()
+
+    def unir_grupo(self, _nombreg1, _nombreg2):
+        query = "MATCH (g:Grupo {nombre:$nomg1}), (c:Grupo {nombre:$nomg2}) CREATE (g)-[:contiene_m]->(c)"
+        self.__sesion.run(query, parameters = {'nomg1':_nombreg1, 'nomg2':_nombreg2})
+
+    def obtener_grupo(self, _nombre):
+        query = "MATCH (Grupo)-[:contiene_a]->(Concepto) WHERE Grupo.nombre=$nom RETURN Grupo.nombre, Concepto.nombre"
+        a = self.__sesion.run(query, parameters = {'nom':_nombre})
+        temp = list()
+        for i in a:
+            temp.append(i)
+        return temp
         
 
        
         
-
-
-
-
 if __name__ == '__main__':
-    a = CGroupRepository('bolt://100.26.171.12:33429', 'neo4j', 'verb-designation-abusers')
+    a = CGroupRepository('bolt://52.207.60.220:33184', 'neo4j', 'heights-completions-preferences')
     a.conectar()
-    #a.crear_grupo('grupo1', 'grupo_de_prueba', str(122234))
-    #a.agregar_concepto(str(122234),'int', '1s3d4', 'primer concepto')
+    #a.crear_grupo('corona_virus', 'muerte_inminente')
+    #a.agregar_concepto('corona_virus', 'fiebre', 'sintoma')
+    #a.crear_grupo('medicamentos_corona', 'tratamiento')
+    #a.agregar_concepto('medicamentos_corona', 'acetaminofen', 'medicamento_efectivo')
+    #a.unir_grupo('corona_virus', 'medicamentos_corona')
+    #a.agregar_concepto('corona_virus', 'tos', 'sintoma2')
+    b = a.obtener_grupo('corona_virus')
+    print(b[0]["Grupo.nombre"])
     #a.eliminar_grupo(str(122234))
     #a.eliminar_concepto('1s3d4')
     #a.obtener_grupo(str(122234))
